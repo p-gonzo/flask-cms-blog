ea69516e877fabd8095bc3ea15ffbf36a11cffce
Phil Gonzalez <pgonzalez115@gmail.com>
2019-12-29 22:51:23 -0700

    update config for heroku postgres

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if os.environ['RUN_ENVIRON'] == 'sqlite3':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % os.path.join(APP_DIR, 'blog.db')
 elif os.environ['RUN_ENVIRON'] == 'local':
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(user="postgres",pw="docker",url="localhost",db="postgres")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(user=os.environ["DB_USER"],pw=os.environ["DB_PASS"],url=os.environ["DB_URL"],db=os.environ["DB_NAME"])
+elif os.environ['RUN_ENVIRON'] == 'heroku':
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 app.config['ADMIN_PASSWORD'] = os.environ['ADMIN_PASSWORD']
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
